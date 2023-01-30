7696dc2016610bfe68be3eae88be8f8831e300d5
Erick <erickfons00@gmail.com>
2023-01-30 06:48:12 -0500

    feat: adding story service

---
+++ src/services/cohere/StoryService.ts
@@ -0,0 +1,56 @@
+import { BaseCohereService } from "./BaseCohereService";
+
+
+export class StoryService{
+  static async generateStoryStart(biome: string, character: string, card: string, entity: string){
+    const { generations } = await BaseCohereService.generate({
+      prompt: `Generate a short story told at the beginning of a dungeons and dragons turn. Leave the story unfinished.
+--
+Biome: "The Highlands"
+Character: "Saldur"
+Card: "hostile"
+Entity: "Harpy"
+Story:
+Far await into the badlands our hero Saldur is ambushed by a bunch of Harpys that comes out of nowhere. Saldur is deeply afraid but ...
+--
+Biome: "Icecrown Citadel"
+Character: "Momon"
+Card: "npc"
+Entity: "Ander the Elf"
+Story:
+Momon enters a huge hall at the entrance of the Icecrown Citadel. Its Ander, a blue skinned elf that comes to meet him. Talks a forgotten language but...
+--
+Biome: "${biome}"
+Character: "${character}"
+Card: "${card}"
+Entity: "${entity}"
+Story:
+`,
+      max_tokens: 300,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      p: 0.75,
+      model: 'xlarge',
+      stop_sequences: ['--'],
+      temperature: 1
+    })
+
+    return generations[0].text;
+  }
+
+  static async generateStoryEnding(historyStart:string, diceResult: number,character:string){
+    const { generations } = await BaseCohereService.generate({
+      prompt: `Write the end of the following story where ${character} ${diceResult}.
+${historyStart}`,
+      max_tokens: 150,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      p: 0.75,
+      model: 'command-xlarge-nightly',
+      stop_sequences: ['--'],
+      temperature: 0.6
+    })
+
+    return generations[0].text;
+  }
+}
--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,3 +1,6 @@
+import classnames from 'classnames'
+
+
 export const getLetterIndex = (letter: string)=>{
   if(letter.length > 1 ) throw new Error('Letter must be a single character')
   if(letter === '^') return 0
@@ -8,4 +11,17 @@ export const getLetterFromIndex = (index: number)=>{
   if(index > 26 || index < 0) throw new Error('Index doesn\'t correspond to a letter')
   if(index === 0) return '^'
   return String.fromCharCode(index - 1 + 'a'.charCodeAt(0))
+}
+
+export const diceResultFromDiff = (dif:number)=>{
+  return classnames({
+    'easily wins': dif >= 10,
+    'wins': dif >= 5 && dif < 10,
+    'barely wins': dif > 0 && dif < 5,
+    'draw': dif === 0,
+    'barely loses': dif < 0 && dif > -5,
+    'loses': dif <= -5 && dif > -10,
+    'easily loses': dif <= -10 && dif > -15,
+    'dies': dif <= -15,
+  })
 }
