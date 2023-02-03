7a92f7b8a5472d9c7d713cbd26117d4209fc6c64
Erick <erickfons00@gmail.com>
2023-02-03 09:24:12 -0500

    feat: adding cover picture

--- src/pages/InitPage.tsx
+++ src/pages/InitPage.tsx
@@ -1,12 +1,17 @@
 import { useRef } from "react"
 import { useNavigate } from "react-router-dom"
+import coverURL from '../pictures/cver.png'
 
 export const InitPage : React.FC = ()=>{
   const characterRef = useRef(JSON.parse(window.localStorage.getItem('character') ?? 'null'))
   const navigate = useNavigate()
 
   return <div className="grid place-content-center h-full">
-    <div className="max-w-xl h-full w-full flex flex-col gap-y-5 py-12">
+    <div className="max-w-2xl h-full w-full flex flex-col gap-y-5 py-12">
+      <div className="relative mb-12">
+        <img src={coverURL} className="relative rounded-md z-20"/>
+        <img src={coverURL} className="absolute rounded-md inset-0 blur-2xl z-10 animate-pulse"/>
+      </div>
       <button onClick={()=>{
         navigate('/character')
       }} className="px-6 py-4 rounded-md ring-offset-4 ring-offset-gray-900 bg-red-500 text-white hover:ring-4 hover:ring-red-500 font-cubano text-2xl">NEW GAME</button>
